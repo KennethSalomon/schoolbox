1607b6d5b378e6d2ce630fcc4299f8639a86aa70
KPANOUkenneth <yissekpanou96@gmail.com>
2026-03-17 09:45:33 +0100

    one depot

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -5,7 +5,6 @@ const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
-// Pour les composants client (hook-based)
 export function createClientComponentClient() {
   return createClient(supabaseUrl, supabaseAnonKey);
 }
