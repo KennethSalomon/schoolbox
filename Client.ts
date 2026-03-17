4e8c026f896e03d7afa107b9d61848e8251f9228
KPANOUkenneth <yissekpanou96@gmail.com>
2026-03-17 04:39:06 +0100

    second post

---
+++ Client.ts
@@ -0,0 +1,11 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+// Pour les composants client (hook-based)
+export function createClientComponentClient() {
+  return createClient(supabaseUrl, supabaseAnonKey);
+}
